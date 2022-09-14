7bbe76d4d4b5c9557645a63aaf9ab26cd7a19032
Marcelo Abreu <marcelo.abreu@valtech.com>
2022-09-14 09:37:06 -0300

    Consuming Promises with Asyng Await

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,24 +1,43 @@
-const fs = require('fs');
-const superagent = require('superagent')
+const fs = require("fs");
+const superagent = require("superagent");
 
-const readFilePro = file =>{
-    return new Promise((resolve, reject)=>{
-        fs.readFile(file, (err, data) =>{
-            if (err) reject(' I could not find that file');
-            resolve(data);
-        });
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject(" I could not find that file");
+      resolve(data);
     });
+  });
 };
-const writeFilePro = (file, data)=>{
-    return new Promise((resolve, reject)=>{
-        fs.writeFile(file, data, err =>{
-            if (err) reject('I coudl not write file :c');
-            resolve('Success');
-        });
-    } );
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject("I coudl not write file :c");
+      resolve("Success");
+    });
+  });
 };
 
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
 
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    console.log(res.body.message);
+    await writeFilePro("dog-imgg.txt", res.body.message);
+    console.log("Random dog image saved sucessfull");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getDogPic();
+
+/*
 readFilePro(`${__dirname}/dog.txt`)
     .then(data => {
     console.log(`Breed: ${data}`);
@@ -32,5 +51,6 @@ readFilePro(`${__dirname}/dog.txt`)
         console.log('Random dog image sabed to file!');
     })  
     .catch(err => {
-        console.log(err.message);
+        console.log(err);
     });
+*/
---
+++ 4-natours/starter/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.status(200).json({message: 'Hello From the server side!',
+    app: "Natours"});
+
+});
+
+app.post('/', (req, res) =>{
+    res.send('You can post to this endpoint...');
+})
+
+const port = 3000;
+app.listen(port, ()=>{
+    console.log(`App runing on port ${port} ...`);
+});
+
